43cb01a7ece909d2497f26fe35a8c97516ae6fe8
HenriqueRib <ribeiro.henriquem@gmail.com>
2022-09-04 12:38:38 -0300

    Retirando funcionalidade de cookies.. Apenas se tiver página de politica de privacidade

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,20 +1,21 @@
-window.cookieconsent.initialise({
-    palette: {
-        popup: {
-            background: "#252e39",
-        },
-        button: {
-            background: "transparent",
-            text: "white",
-            border: "white",
-        },
-    },
-    content: {
-        message:
-            "Este site utiliza cookies para melhorar a sua experiência online e analisar o trafégo do nosso site. Ao clicar em “Aceitar”, você concorda com a nossa Política de Privacidade.",
-        dismiss: "Aceitar",
-        deny: "Recusar",
-        link: "Leia mais",
-        href: "www.renanplast.com.br/privacidade",
-    },
-});
+// Caso haja página de politica de privacidade
+// window.cookieconsent.initialise({
+//     palette: {
+//         popup: {
+//             background: "#252e39",
+//         },
+//         button: {
+//             background: "transparent",
+//             text: "white",
+//             border: "white",
+//         },
+//     },
+//     content: {
+//         message:
+//             "Este site utiliza cookies para melhorar a sua experiência online e analisar o trafégo do nosso site. Ao clicar em “Aceitar”, você concorda com a nossa Política de Privacidade.",
+//         dismiss: "Aceitar",
+//         deny: "Recusar",
+//         link: "Leia mais",
+//         href: "www.renanplast.com.br/privacidade",
+//     },
+// });
